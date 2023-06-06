b3cd1139ea9553a509d0b09f73bc1396a412d6bd
NTSMOBILE\dmatilla <dmatilla@nts-solutions.com>
2023-06-06 14:06:22 +0200

    Fix for notifyDataSetChanged method

--- library/src/main/java/com/cleveroad/adaptivetablelayout/AdaptiveTableLayout.java
+++ library/src/main/java/com/cleveroad/adaptivetablelayout/AdaptiveTableLayout.java
@@ -1637,12 +1637,8 @@ public class AdaptiveTableLayout extends ViewGroup implements ScrollHelper.Scrol
 
     @Override
     public void notifyDataSetChanged() {
-        recycleViewHolders(true);
-        mVisibleArea.set(mState.getScrollX(),
-                mState.getScrollY(),
-                mState.getScrollX() + mSettings.getLayoutWidth(),
-                mState.getScrollY() + mSettings.getLayoutHeight());
-        addViewHolders(mVisibleArea);
+        initItems();
+        notifyLayoutChanged();
     }
 
     @Override
